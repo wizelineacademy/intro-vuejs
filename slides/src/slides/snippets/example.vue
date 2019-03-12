3640f4c2da39d1beb398d4c2cc3a780c642b1c77
Eder Díaz <ederchrono@gmail.com>
2019-03-11 18:24:24 -0600

    📝 add images and slides

---
+++ slides/src/slides/snippets/example.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="welcome">
+    <h1>Hello {{ message }}!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "world"
+    };
+  }
+};
+</script>
+
+<style>
+.welcome {
+  color: red;
+}
+</style>
